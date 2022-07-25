e425c5f0e2d2bf8955a11882d30dac66ca09f3aa
没礼貌的芬兰人 <liao520ly@gmail.com>
2022-07-25 16:50:28 +0800

    fix build update md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # spx-translation-chrome
-聚合翻译谷歌插件版本
+翻译简单插件,让翻译网页只做翻译这件事情,默认设置背景图...
+桌面端实现截屏翻译 [传送门](https://github.com/mlmdflr/spx-translation)
+
+## support
+- [x] 谷歌翻译
+- [x] deepl
+- [ ] 有道
--- build.js
+++ build.js
@@ -13,7 +13,7 @@ existsSync(zip) && unlinkSync(zip)
 existsSync(LICENSE) && copyFileSync(LICENSE, join(resolve('dist'), 'LICENSE'))
 
 try {
-    execSync(`${resolve('7z/7za.exe')} a ${zip} ${resolve('dist')} -mx=9 `)
+    execSync(`${resolve('7z/7za.exe')} a ${zip} ${resolve('dist')}/* -mx=9 `)
     console.log('\x1B[32m[build success] \x1B[0m');
 } catch (error) {
     console.error(error);
