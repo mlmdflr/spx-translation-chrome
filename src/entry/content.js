f21e9cb0378d102200293d167eaa69bf4a793375
没礼貌的芬兰人 <liao520ly@gmail.com>
2022-09-07 09:01:07 +0800

    all done

--- src/entry/content.js
+++ src/entry/content.js
@@ -1,7 +1,8 @@
 import { init as comInit } from "../google/com/js/index.js";
 import { init as cnInit } from "../google/cn/js/index.js";
 
-import { init as deeplComInit } from "../deepl/com/js/index.js";
+import { init as deeplComInit } from "../deepl/js/index.js";
+
 
 switch (location.host) {
     case 'translate.google.com':
